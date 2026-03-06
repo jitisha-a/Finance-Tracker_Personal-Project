204d9a20be82f488aa4b6b3bb9623a940d21b8ec
jitisha-a <140800240+jitisha-a@users.noreply.github.com>
2026-03-06 22:00:43 +0800

    Rename CSV file to sample_finance_data.csv

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from data_entry import get_amount, get_category, get_date, get_description
 import matplotlib.pyplot as plt
 
 class CSV:
-    CSV_FILE = "finance_data.csv"
+    CSV_FILE = "sample_finance_data.csv"
     COLUMNS = ["date", "amount", "category", "description"]
     FORMAT = "%d-%m-%Y"
 
